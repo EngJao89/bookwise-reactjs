63f6a09903bd77f90a4530e22f51ff7e9ed2f045
EngJao89 <eng_joao@outlook.com>
2024-02-14 22:37:24 -0300

    feat(bw03-login): screen login implementation

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image"
+
+import backgroundImg from "../../../public/hero.png";
+import logoGoogle from "../../../public/logos_google-icon.svg";
+import logoGitHub from "../../../public/akar-icons_github-fill.svg";
+import logoRocket from "../../../public/RocketLaunch.svg";
+
+import { 
+  CardLogin,
+  LoginContainer,  
+  MainSection, 
+  SubTitleSection
+} from "./styles"
+import Link from "next/link";
+
+export default function Login(){
+  return (
+    <LoginContainer>
+      <Image alt="Logo" src={backgroundImg}/>
+
+      <MainSection>
+        Boas vindas!
+        <SubTitleSection>
+          Faça seu login ou acesse como visitante.
+        </SubTitleSection>
+        <CardLogin>
+          <Image alt="Google" src={logoGoogle}/>
+          Entrar com Google
+        </CardLogin>
+        <CardLogin>
+          <Image alt="GitHub" src={logoGitHub}/>
+          Entrar com GitHub
+        </CardLogin>
+
+        <Link href="/" passHref legacyBehavior>
+          <CardLogin>
+            <Image alt="Rocket" src={logoRocket}/>
+            Acessar como visitante
+          </CardLogin>
+        </Link>
+      </MainSection>
+    </LoginContainer>
+  )
+}
